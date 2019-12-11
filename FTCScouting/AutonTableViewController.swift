3d8beee2d1dbb24ae691a415eccc69ff16040a7e
Wesley Vong <wv@vongs.com>
2019-12-10 16:43:44 -0800

    Changed Model Object to fit FTC

---
+++ FTCScouting/AutonTableViewController.swift
@@ -0,0 +1,9 @@
+//
+//  AutonTableViewController.swift
+//  FTCScouting
+//
+//  Created by Wesley Vong on 12/5/19.
+//  Copyright © 2019 Takahashi, Alex. All rights reserved.
+//
+
+import Foundation
